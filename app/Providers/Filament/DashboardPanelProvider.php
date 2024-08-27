5bba87c3b6bdbb0f9f1bf4232a8130f94fb1b577
Sahrul Romadi <sahrulromadi12@gmail.com>
2024-08-27 21:57:44 +0700

    add spatie roles and permission

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -2,21 +2,28 @@
 
 namespace App\Providers\Filament;
 
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
-use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
 use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Filament\PanelProvider;
+use Filament\Pages\Dashboard;
+use Filament\Support\Colors\Color;
+use Filament\Navigation\NavigationItem;
+use Filament\Navigation\NavigationGroup;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Navigation\NavigationBuilder;
+use App\Filament\Resources\ArticleResource;
+use App\Filament\Resources\CampaignResource;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
 
 class DashboardPanelProvider extends PanelProvider
 {
@@ -53,6 +60,35 @@ class DashboardPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->plugin(FilamentSpatieRolesPermissionsPlugin::make())
+            ->navigation(function (NavigationBuilder $builder): NavigationBuilder {
+                return $builder->groups([
+                    NavigationGroup::make()
+                        ->items([
+                            NavigationItem::make('Dashboard')
+                                ->icon('heroicon-o-home')
+                                ->isActiveWhen(fn(): bool => request()->routeIs('filament.dashboard.pages.dashboard'))
+                                ->url(fn(): string => Dashboard::getUrl()),
+                        ]),
+                    NavigationGroup::make('Content')
+                        ->items([
+                            ...ArticleResource::getNavigationItems(),
+                            ...CampaignResource::getNavigationItems(),
+                        ]),
+                    NavigationGroup::make('Setting')
+                        ->items([
+                            NavigationItem::make('Role')
+                                ->icon('heroicon-o-user-group')
+                                ->isActiveWhen(fn(): bool => request()->routeIs('filament.dashboard.resources.roles.*'))
+                                ->url(fn(): string => '/dashboard/roles'),
+
+                            NavigationItem::make('Permission')
+                                ->icon('heroicon-o-lock-closed')
+                                ->isActiveWhen(fn(): bool => request()->routeIs('filament.dashboard.resources.permissions.*'))
+                                ->url(fn(): string => '/dashboard/permissions'),
+                        ]),
+                ]);
+            });
     }
 }
